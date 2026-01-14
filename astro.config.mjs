9a49580b8523dc9e75e756f22fdfb4e39a7831e7
Ruben Boogaard <rubenboogaard@MacBook-Air-van-Ruben.local>
2026-01-14 11:24:22 +0100

    Fix: Removed /nl/ redirect, moved Dutch pages to root

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
     defaultLocale: "nl",
     locales: ["nl", "en", "de"],
     routing: {
-      prefixDefaultLocale: true
+      prefixDefaultLocale: false
     }
   }
 });
